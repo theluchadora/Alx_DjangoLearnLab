71d9fb39f4eb59ff3a9303baab564715dbf67323
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-09-06 02:55:46 +0300

    fix import structure

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
 from .models import Library
 from .models import Book
+from django.views.generic.detail import DetailView
 
 
 # Function-based view to list all books
