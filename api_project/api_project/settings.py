2709914248a534ddc2d50d4123f2634f3d83973d
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-09-19 20:33:49 +0300

    task 3 completed

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,8 +38,20 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',  # optional
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',  # require auth by default
+    ],
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # app urls
     path('api/', include('api.urls')),
+    # token endpoint
+    path('api-token-auth/', views.obtain_auth_token, name='api_token_auth'),
 ]
